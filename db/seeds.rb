4a92424421dba335b2dcb228961e78bd58941e4c
prativa <prativa.pokhrel95@gmail.com>
2023-05-03 00:31:07 -0400

    adding testing data using faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,22 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require 'faker'
+I18n.reload!
 
+Product.destroy_all
 
+10.times do |index|
+    Product.create!(
+      name: Faker::Vehicle.make_and_model,
+      manufacturer: Faker::Vehicle.manufacture,
+      style: ['Road Bike', 'Mountain Bike', 'City Bike'].sample,
+      purchase_price: rand(500..1000),
+      sale_price: rand(1000..2000),
+      qty_on_hand: rand(0..10),
+      commission_percentage: rand(5..10)
+    )
+  end
+  
+
+p "Created #{Product.count} products"
