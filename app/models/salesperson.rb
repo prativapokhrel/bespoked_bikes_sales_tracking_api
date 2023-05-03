f1c19f4b60451f815915d2c6ad64fbad564b2970
prativa <prativa.pokhrel95@gmail.com>
2023-05-03 14:22:23 -0400

    salesperson model and request test cases

--- app/models/salesperson.rb
+++ app/models/salesperson.rb
@@ -3,4 +3,7 @@ class Salesperson < ApplicationRecord
   has_many :sales 
 
   validates_uniqueness_of :phone
+
+  validates_presence_of :first_name, :last_name, :start_date, :phone, :manager
+
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -9,13 +9,20 @@ FactoryBot.define do
       commission_percentage {rand(5..10)}
     end
 
-    # factory :product do
-    #     name { Faker::Vehicle.make_and_model }
-    #     manufacturer { Faker::Vehicle.manufacture }
-    #     style {['Road Bike', 'Mountain Bike', 'City Bike'].sample}
-    #     purchase_price {rand(500..1000)}
-    #     sale_price {rand(1000..2000)}
-    #     qty_on_hand {rand(0..10)}
-    #     commission_percentage {rand(5..10)}
-    #   end
+    factory :manager do 
+        first_name { Faker::Name.first_name }
+        last_name { Faker::Name.last_name }
+        email { Faker::Internet.email }
+    end 
+
+    factory :salesperson do
+        association :manager 
+        first_name { Faker::Name.first_name }
+        last_name { Faker::Name.last_name }
+        phone {Faker::PhoneNumber.cell_phone}
+        address { Faker::Address.full_address }
+        start_date { Faker::Date.backward(days: 365) }
+        termination_date { Faker::Date.forward(days: 365) }
+        manager_id {[Manager.first.id, Manager.last.id].sample}
+    end
   end
