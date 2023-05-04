1bd422e394510d9b265db3f26d2b788ae43caabc
prativa <prativa.pokhrel95@gmail.com>
2023-05-04 02:55:42 -0400

    sales person commission routes + action

--- app/controllers/api/v1/sales_controller.rb
+++ app/controllers/api/v1/sales_controller.rb
@@ -4,17 +4,27 @@ class Api::V1::SalesController < ApplicationController
         if params[:from_date] && params[:to_date]
             from_date = Date.parse(params[:from_date])
             to_date = Date.parse(params[:to_date])
-            @sales = Sale.where(sales_date: from_date.beginning_of_day..to_date.end_of_day)
+            @sales = Sale.where(sales_date: from_date.beginning_of_day..to_date.end_of_day).order(sales_date: :asc)
         else 
-            @sales = Sale.all 
+            @sales = Sale.all.order(sales_date: :asc)
         end 
-       
-        render json: @sales, status: :ok 
+        render json: @sales.as_json(methods: [:salesperson_commission, :discounted_price]),  status: :ok 
     end 
 
     def show 
         @sale = Sale.find(params[:id])
         render json: @sale, status: :ok 
+    end 
+
+    def chart 
+        @sales = []
+        Sale.all.each do |sale|
+            @sales << {sale_price: sale.discounted_price, 
+                salesperson: "#{sale.salesperson.first_name} #{sale.salesperson.last_name}",
+                sales_date: sale.sales_date}
+        end 
+
+        render json: @sales.sort_by { |data| data[:sales_date] }, status: :ok
     end 
 
     def create 
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   namespace :api do 
     namespace :v1 do 
+
+      get 'sales/chart' => 'sales#chart'
       resources :products, only: [:index, :update, :show]
       resources :salespeople, only: [:index, :update, :show]
       resources :customers, only: [:index, :show, :update]
@@ -9,7 +11,7 @@ Rails.application.routes.draw do
       resources :managers, only: [:index, :show, :update]
       resources :discounts, only: [:index, :show, :update]
 
-      get 'salespeople/quarterly_commission_report' => 'salespeople#quarterly_commission_report'
+      get 'quarterly_commission_report' => 'salespeople#quarterly_commission_report'
     end 
   end 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
